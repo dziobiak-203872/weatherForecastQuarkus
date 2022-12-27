72d0049220601cd1b76bdd6e81eea1f6a2cccddd
Aleksandra Dziobiak <olad85@interia.pl>
2022-12-27 21:06:57 +0100

    weather provider and forecast generated services

---
+++ weather-forecast-service/src/test/java/org/acme/wforecast/WeatherForecastResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.wforecast;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class WeatherForecastResourceIT extends WeatherForecastResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ weather-provider-service/src/test/java/org/acme/wprovider/WeatherProviderResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.wprovider;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class WeatherProviderResourceIT extends WeatherProviderResourceTest {
+    // Execute the same tests but in packaged mode.
+}
