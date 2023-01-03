30274f5a2f008d1bdc2e850e9c565ea484e37bcc
dziobiak-203872 <olad85@interia.pl>
2023-01-03 20:56:52 +0100

    weather provider service tests

--- weather-provider-service/src/test/java/org/acme/wprovider/WeatherProviderResourceTest.java
+++ weather-provider-service/src/test/java/org/acme/wprovider/WeatherProviderResourceTest.java
@@ -5,17 +5,33 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasSize;
 
 @QuarkusTest
 public class WeatherProviderResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testGetWeatherProviderById() {
+        String existingProvider = "weatherapi";
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .pathParam("providerId", existingProvider)
+                .when().get("/api/providers/{providerId}")
+                .then()
+                .statusCode(200)
+                .body("url", is("http://api.weatherapi.com/v1/current.json?"))
+                .body("$", hasKey("apiKey"))
+                .body("params", hasSize(1))
+                .body("delimiter", is(","));
     }
 
+    @Test
+    public void testGetWeatherProviderByIdNegative() {
+        String nonExistentProvider = "nonexistentapi";
+        given()
+                .pathParam("providerId", nonExistentProvider)
+                .when().get("/api/providers/{providerId}")
+                .then()
+                .statusCode(204);
+    }
 }
